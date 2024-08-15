ea46d14aecb8a8a824f00c08eefed94472fb2ee9
Fishermanz <839874234@qq.com>
2024-08-15 13:19:22 +0800

    feat: Update fill_blank function call in main.py
    Update the fill_blank function call in main.py to pass the answerList dictionary as the argument instead of a list. This change ensures that the function works correctly and fills the blank with the correct value.
    
    Recent user commits:
    - Fix fill_blank function call in main.py
    - Update ChromeOptions in main.py for headless mode and proxy server
    - Add lazy loading for images in main.py
    - Update main.py to use CSS selector for submit button
    
    Recent repository commits:
    - Update IP retrieval method in main.py
    - Add numpy to requirements.txt
    - Add selenium and requests to requirement.txt
    - 更新Python版本为3.9.12，并添加定时任务工作流程和配置文件
    - 添加定时任务工作流程和配置文件
    - Initial commit

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ prob = {
 }
 # 简答题题库
 answerList = {
-    2: ['?']
+    2: ['你好']
 }
 # IP API提取链接 https://xip.ipzan.com/ 这个每周都有几百个免费的IP代理领取
 api = ""
@@ -15,3 +15,6 @@ UA = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like
       "Mozilla/5.0 (Linux; Android 10; SEA-AL10 Build/HUAWEISEA-AL10; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/4313 MMWEBSDK/20220805 Mobile Safari/537.36 MMWEBID/9538 MicroMessenger/8.0.27.2220(0x28001B53) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64",
       "Mozilla/5.0 (Linux; Android 11; Redmi Note 8 Pro Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/89.0.4389.72 MQQBrowser/6.2 TBS/045913 Mobile Safari/537.36 V1_AND_SQ_8.8.68_2538_YYB_D A_8086800 QQ/8.8.68.7265 NetType/WIFI WebP/0.3.0 Pixel/1080 StatusBarHeight/76 SimpleUISwitch/1 QQTheme/2971 InMagicWin/0 StudyMode/0 CurrentMode/1 CurrentFontScale/1.0 GlobalDensityScale/0.9818182 AppId/537112567 Edg/98.0.4758.102",
       ]
+
+
+print(answerList)
